576afe9bc73eac96986be5cc8787fdb42e2ab5cf
Izymryda <witebear200396@gmail.com>
2022-01-14 14:17:52 +0200

    Create a services lay

--- LovePlanet.Domain.Core/Relations.cs
+++ LovePlanet.Domain.Core/Relations.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace LovePlanet.Domain.Core
 {
-    class Relations
+    public class Relations
     {
         public int Id { get; set; }
         public string Name { get; set; }
---
+++ LovePlanet.Services.Interfaces/IRel_of_pep.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using LovePlanet.Domain.Core;
+
+namespace LovePlanet.Services.Interfaces
+{
+    public interface IRel_of_pep
+    {
+        void MakeRel_of_pep(People people1, People people2, Relations relations);
+    }
+}
